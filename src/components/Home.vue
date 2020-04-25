5f2fffcf0ac446da0d87558d7a3aac1198aeb187
Agung Hari Wijaya <a9un9hari@gmail.com>
2020-04-26 06:41:36 +0700

    Add maps on home

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="map">
+      <h2>Peta Laporan Desk Relawan</h2>
+      <div style="height: 600px; width: 100%">
+        <l-map :zoom="zoom" :center="center">
+          <l-marker :lat-lng="center"></l-marker>
+          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        </l-map>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 12,
+      center: [-8.101452, 112.160277],
+      bounds: null,
+    };
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import login from '@/components/login'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/login',
