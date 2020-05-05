bc9f59037676aba9d210fbcac829211066a71e0e
Agung Hari Wijaya <a9un9hari@gmail.com>
2020-05-05 23:52:37 +0700

    fixing login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,22 +1,22 @@
 <template>
 <div class="flex justify-center">
     <div class="w-full max-w-xs">
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form @submit.prevent="login" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                     Username
                 </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                     Password
                 </label>
-                <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
-                <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                <input v-model="password" class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Sign In
                 </button>
                 <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
@@ -30,3 +30,42 @@
     </div>
 </div>
 </template>
+
+<script>
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+export default {
+  name: 'Login.vue',
+
+  data: () => ({
+    email : '',
+    password : ''
+  }),
+
+  mounted() {
+
+  },
+
+  methods:{
+    async login() {
+        const { data } = await axios.post('http://lumenvue.test/login', {
+            email : this.email,
+            password : this.password
+        })
+        .then( 
+            (response) => { console.log(response) },
+            (error) => { console.log(error) }
+        );
+        console.log('as');
+
+    //   const apiToken = data.api_token;
+
+    //   Cookies.set('token', apiToken);
+
+    //   this.$router.push('/')
+    }
+  }
+
+}
+</script>
